2f426bd1c9ef90bea2b0beeb133b5b8680ca8890
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-09-12 22:10:51 -0500

    Initial file

--- testfile-02/main.cpp
+++ testfile-02/main.cpp
@@ -6,9 +6,46 @@
 //
 
 #include <iostream>
+#include <iterator>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+const std::vector <int> default_contents = { 100, 200, 300, 0, 420, 0, 0, 0, 1, 1, 2, 3, 5, 8, 0, 1000 };
+
+template <typename ITERATOR>
+int range_sum(ITERATOR begin, ITERATOR end) {
+    int sum = 0;
+    for (auto it = default_contents.begin(); it != default_contents.end(); it++)
+        sum += *it;
+    return sum;
+}
+
+template <typename ITERATOR>
+    double range_avg(ITERATOR begin, ITERATOR end) {
+    double sum = 0;
+    double avg = 0;
+    int range_divisor = (int)default_contents.size();
+    for (auto it = default_contents.begin(); it != default_contents.end(); it++)
+        sum += *it;
+    avg = sum / range_divisor;
+    
+    return avg;
+    
+}
+
+template <typename ITERATOR>
+auto range_maxval(ITERATOR begin, ITERATOR end) {
+    
+
+}
+
+
+int main()
+{
+    std::cout << "the range sum is: " << std::endl;
+    std::cout << range_sum(default_contents.begin(), default_contents.end());
+    std::cout << std::endl;
+    std::cout << range_avg(default_contents.begin(), default_contents.end());
+    std::cout << std::endl;
+    //std::cout << range_maxval(default_contents.begin(), default_contents.end());
     return 0;
 }
